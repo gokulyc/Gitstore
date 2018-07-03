58f6ae5152ed2df0636fd088214cfc4a42fac7a9
gokulyc <gokulyc@gmail.com>
2018-07-03 23:06:37 +0530

    executed 1

--- Webapi_net/WebApiexample/WebApiexample/Controllers/ValuesController.cs
+++ Webapi_net/WebApiexample/WebApiexample/Controllers/ValuesController.cs
@@ -9,31 +9,38 @@ namespace WebApiexample.Controllers
 {
     public class ValuesController : ApiController
     {
+        static List<string> strings = new List<string>()
+        {
+            "str1","str2","str3"
+        };
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return strings;
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return strings[id];
         }
 
         // POST api/values
         public void Post([FromBody]string value)
         {
+            strings.Add(value);
         }
 
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
+            strings[id]= value;
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            strings.RemoveAt(id);
         }
     }
 }
