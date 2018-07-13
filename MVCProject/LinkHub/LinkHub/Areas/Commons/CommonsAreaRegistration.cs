07217abe5f2ef4acca47cef6403f11e03b4c9b8a
gokulyc <gokulyc@gmail.com>
2018-07-13 16:00:57 +0530

    areas added

---
+++ MVCProject/LinkHub/LinkHub/Areas/Commons/CommonsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace LinkHub.Areas.Commons
+{
+    public class CommonsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Commons";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Commons_default",
+                "Commons/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
